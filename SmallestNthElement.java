a7fb34d13a12fad38281907a7dc52bc50a07c800
Divij <divij.shrivastava@gmail.com>
2022-08-29 15:19:15 +0530

    finding smallest Nth number in array

--- SmallestNthElement.java
+++ SmallestNthElement.java
@@ -1,36 +1,37 @@
-import java.util.ArrayList;
 
 public class SmallestNthElement {
- 
+
     public static void main(String[] args) {
 
-        int [] arr = new int[]{1, 2, 0, 4, 5, 7};
+        int[] arr = new int[] { 4, 9, 10, 2, 5, 7 };
+        int smallNum = 3;
+        for (int i = 0; i < smallNum; i++) {
 
-        ArrayList<Integer> arrList = new ArrayList<>();
+            int minIndex = findMin(arr, i);
+            swap(minIndex, i, arr);
 
-        for(int a: arr){
-            arrList.add(a);
         }
 
-        int smallNum = 1;
-        int min = Integer.MAX_VALUE;
-        for(int j = 0;j<smallNum; j++){
-            min = arrList.get(0);
-            int minIndex = 0;
-            for(int i = 0; i<arrList.size(); i++){
-                
-                if(arrList.get(i)<min){
-                    min = arrList.get(i);
-                    minIndex = i;
-                }
+        System.out.println(arr[smallNum - 1]);
 
-            }
-            arrList.remove(minIndex);
-        }
-
-        System.out.println(min);
-            
     }
-        
-}
 
+    private static void swap(int index1, int index2, int[] arr) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    private static int findMin(int[] arr, int startIndex) {
+        int minIndex = startIndex;
+        int min = arr[minIndex];
+        for (int i = startIndex; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+}
